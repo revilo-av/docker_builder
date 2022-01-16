f184b80486ffe65cd9e38a814e4a5011fde82739
revolo <oliver.avarello@gmail.com>
2022-01-16 18:57:14 +0100

    fix config

--- create_image.sh
+++ create_image.sh
@@ -1,11 +1,11 @@
 
 
-imageName="sonar"
+imageName="test:latest"
 
-docker_result=$(docker container ls -a | grep ${imageName}* |wc -l)
+docker_result=$(docker container ls -a | grep ${imageName} |wc -l)
 #docker_result=$(docker container ls -a | grep test:latest |wc -l)
 
-echo $docker_result
+echo "anzahl der vorhandenen Containern" $docker_result
 
 if [ $docker_result -eq 1 ]
 then
@@ -14,12 +14,20 @@ echo Contaienr gefunden mit dem namen .
 
 container_id=$(docker container ls -aq --filter name=test*)
 
+
+echo "container id die zu löschen ist $container_id"
+
 docker stop $container_id
 
 echo Contaienr mit dem namen "container namen wurde beendet"
 
 
+
 docker rm  $container_id
+
+docker_image_id=$(docker images 'test:latest' -a -q)
+
+docker image rm -f $docker_image_id
 
 else
 
@@ -27,7 +35,7 @@ echo keine container gefunden
 
 fi
 
-#docker build -t test:latest .
+docker build -t test:latest .
 
 
-#docker run -d --name test test:latest
+docker run -v /var/run/docker.sock:/var/run/docker.sock -d --name test test:latest # -it sh
--- dockerfile
+++ dockerfile
@@ -1,22 +1,25 @@
 #!/bin/sh
 
 #FROM maven:3.8.4-eclipse-temurin-17-alpine
-FROM maven:3.8.4-eclipse-temurin-17
+FROM maven:3.8.4-eclipse-temurin-17 as builder
 LABEL maintainer "oliver.avarello@meta-level.de"
-
 
 RUN apt-get update && apt-get upgrade -y
 
-#RUN apt install docker.io -y
+RUN apt install docker.io -y
 
 
-WORKDIR /app
+FROM builder
 
 RUN git clone -b main https://github.com/revilo-av/test.git /app/test01
 
+
 WORKDIR /app/test01
 
-#RUN mvn clean compile jib:dockerBuild
-RUN echo "Hallo Welt"
+#RUN git checkout development
 
-CMD ["sleep", "2500"]
+RUN mvn dependency:go-offline
+
+
+#CMD ["sleep", "2500"]
+CMD mvn clean compile jib:dockerBuild
